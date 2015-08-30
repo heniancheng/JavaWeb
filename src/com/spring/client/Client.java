56af0e9281929b1ddace4c26518183da9d13a452
heniancheng <845802942@qq.com>
2015-08-30 22:47:00 +0800

    更新spring的jar库文件

--- src/com/spring/client/Client.java
+++ src/com/spring/client/Client.java
@@ -1,6 +1,6 @@
 package com.spring.client;
 
-import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.spring.manager.UserManager;
@@ -8,7 +8,7 @@ import com.spring.manager.UserManager;
 public class Client {
 	
 	public static void main(String[] args){
-		BeanFactory factory=new ClassPathXmlApplicationContext("applicationContext.xml");
+		ApplicationContext factory=new ClassPathXmlApplicationContext("applicationContext.xml");
 		UserManager manager = (UserManager)factory.getBean("userManager");
 		manager.save("hechen","admin");
 	}
